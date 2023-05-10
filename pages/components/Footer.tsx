42b471c5c4bdebc315fa6d658b44c5978d079018
leesangwon <tkddnjs5613@naver.com>
2023-05-10 18:03:48 +0900

    Design: Footer UI 변경
    - 상단 테두리 radius 속성 추가

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -18,41 +18,43 @@ import { Container } from "@chakra-ui/react";
 
 const Footer = () => {
   return (
-    <>
-      <Divider orientation="horizontal" colorScheme="gray" width={"100%"} />
-      <HStack justify="space-between" paddingY={5}>
-        <Button colorScheme="transparent" size="md" color={"black"}>
-          <Flex direction="column" align="center" gap={1.5}>
-            <Icon as={MdHomeFilled} boxSize={6} color={"grey"} />
-            <span style={{ fontWeight: 400 }}>홈</span>
-          </Flex>
-        </Button>
-        <Button colorScheme="transparent" size="md" color={"black"}>
-          <Flex direction="column" align="center" gap={1.5}>
-            <Icon as={BsCalendarEventFill} boxSize={6} color={"grey"} />
-            <span style={{ fontWeight: 400 }}>시간표</span>
-          </Flex>
-        </Button>
-        <Button colorScheme="transparent" size="md" color={"black"}>
-          <Flex direction="column" align="center" gap={1.5}>
-            <Icon as={HiChatBubbleLeftRight} boxSize={6} color={"grey"} />
-            <span style={{ fontWeight: 400 }}>익명게시판</span>
-          </Flex>
-        </Button>
-        <Button colorScheme="transparent" size="md" color={"black"}>
-          <Flex direction="column" align="center" gap={1.5}>
-            <Icon as={FaClipboardList} boxSize={6} color={"grey"} />
-            <span style={{ fontWeight: 400 }}>정보공유</span>
-          </Flex>
-        </Button>
-        <Button colorScheme="transparent" size="md" color={"black"}>
-          <Flex direction="column" align="center" gap={1.5}>
-            <Icon as={FaUserCircle} boxSize={6} color={"grey"} />
-            <span style={{ fontWeight: 400 }}>내 정보</span>
-          </Flex>
-        </Button>
-      </HStack>
-    </>
+    <HStack
+      justify="space-between"
+      paddingY={5}
+      border={"1px solid #ECECEF"}
+      borderTopRadius={24}
+    >
+      <Button colorScheme="transparent" size="md" color={"black"}>
+        <Flex direction="column" align="center" gap={1.5}>
+          <Icon as={MdHomeFilled} boxSize={6} color={"grey"} />
+          <span style={{ fontWeight: 400 }}>홈</span>
+        </Flex>
+      </Button>
+      <Button colorScheme="transparent" size="md" color={"black"}>
+        <Flex direction="column" align="center" gap={1.5}>
+          <Icon as={BsCalendarEventFill} boxSize={6} color={"grey"} />
+          <span style={{ fontWeight: 400 }}>시간표</span>
+        </Flex>
+      </Button>
+      <Button colorScheme="transparent" size="md" color={"black"}>
+        <Flex direction="column" align="center" gap={1.5}>
+          <Icon as={HiChatBubbleLeftRight} boxSize={6} color={"grey"} />
+          <span style={{ fontWeight: 400 }}>익명게시판</span>
+        </Flex>
+      </Button>
+      <Button colorScheme="transparent" size="md" color={"black"}>
+        <Flex direction="column" align="center" gap={1.5}>
+          <Icon as={FaClipboardList} boxSize={6} color={"grey"} />
+          <span style={{ fontWeight: 400 }}>정보공유</span>
+        </Flex>
+      </Button>
+      <Button colorScheme="transparent" size="md" color={"black"}>
+        <Flex direction="column" align="center" gap={1.5}>
+          <Icon as={FaUserCircle} boxSize={6} color={"grey"} />
+          <span style={{ fontWeight: 400 }}>내 정보</span>
+        </Flex>
+      </Button>
+    </HStack>
   );
 };
 
