d26ab3cd483c1666f2a5c92a1226f351cf6f198d
Jimmy <qny123@naver.com>
2023-05-23 22:34:31 +0900

    feat: header와 navbar를 포함한 layout 컴포넌트 구현

---
+++ components/layout/Layout.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+
+import Header from "./Header";
+import Navbar from "./Navbar";
+
+interface Props {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: Props) {
+  return (
+    <>
+      <Header />
+      {children}
+      <Navbar />
+    </>
+  );
+}
