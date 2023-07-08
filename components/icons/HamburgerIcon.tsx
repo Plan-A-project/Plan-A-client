55877c54298030efc2964eaa3c4189abd73fec9a
Juyeon Kim <kkjuyeon@gmail.com>
2023-07-08 19:52:35 +0900

    feat: 정보공유 모집글

---
+++ components/icons/HamburgerIcon.tsx
@@ -0,0 +1,18 @@
+export default function HamburgerIcon() {
+  return (
+    <svg
+      width="16"
+      height="18"
+      viewBox="0 0 16 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M7 5C7 5.55228 7.44772 6 8 6C8.55228 6 9 5.55228 9 5C9 4.44772 8.55228 4 8 4C7.44772 4 7 4.44772 7 5ZM7 10C7 10.5523 7.44772 11 8 11C8.55228 11 9 10.5523 9 10C9 9.44772 8.55228 9 8 9C7.44772 9 7 9.44772 7 10ZM8 16C7.44772 16 7 15.5523 7 15C7 14.4477 7.44772 14 8 14C8.55228 14 9 14.4477 9 15C9 15.5523 8.55228 16 8 16Z"
+        fill="#75788A"
+      />
+    </svg>
+  );
+}
