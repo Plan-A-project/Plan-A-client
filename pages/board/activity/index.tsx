cc040a74debfe987ed51072fd502c752e54d8c1e
hi6724 <hi6724@gmail.com>
2023-07-17 23:18:51 +0900

    ✨ feat: 대외활동 페이지

--- pages/board/activity/index.tsx
+++ pages/board/activity/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import { Badge } from "@chakra-ui/layout";
 import { useRouter } from "next/router";
@@ -10,26 +10,27 @@ import FreeBoardTab from "@/components/board/FreeBoardTabs";
 import { AppContainer, Header } from "@/components/common";
 import IconSearch from "@/components/icons/IconSearch";
 
-function ClubMain() {
+import { AllPosts } from "../free";
+
+function ActivityMain() {
   return (
     <AppContainer>
-      <Header back title="학교생활" rightNode={<IconSearch />} />
+      <Header back title="대외활동" rightNode={<IconSearch />} />
       <FreeBoardTab
         leftLabel="전체"
-        rightLabel="공지"
+        rightLabel="모집"
         leftTab={<AllPosts />}
-        rightTab={<NoticePosts />}
+        rightTab={<RecruitPosts />}
       ></FreeBoardTab>
       <BoardFAB />
     </AppContainer>
   );
 }
 
-export default ClubMain;
+export default ActivityMain;
 
 type SortType = "recent" | "popular";
-
-export const AllPosts = () => {
+export const RecruitPosts = () => {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
   const [sort, setSort] = useState<SortType>("recent");
@@ -115,6 +116,8 @@ export const AllPosts = () => {
               date="2022-12-12"
               views={el.views}
               title={el.title}
+              leftTag="기업"
+              tagType="primary"
               onClick={() => router.push(`/board/${el.id}`)}
             />
           ))}
@@ -123,98 +126,3 @@ export const AllPosts = () => {
     </div>
   );
 };
-
-function NoticePosts() {
-  const router = useRouter();
-  const [loading, setLoading] = useState(false);
-  const [sort, setSort] = useState<SortType>("recent");
-  const [clubBoardList, setClubBoardList] = useState<any[]>([]);
-  const getBoardList = useCallback(async (type: SortType) => {
-    setLoading(true);
-    setTimeout(() => {
-      setClubBoardList([
-        {
-          id: 1,
-          title: `제목 ${type}`,
-          description: "본문",
-          comments: 24,
-          likes: 3,
-          date: "2022-12-12",
-        },
-        {
-          id: 2,
-          title: `제목 ${type}`,
-          description: "본문",
-          comments: 24,
-          likes: 3,
-          date: "2022-12-12",
-        },
-        {
-          id: 3,
-          title: `제목 ${type}`,
-          description: "본문",
-          comments: 24,
-          likes: 3,
-          date: "2022-12-12",
-        },
-      ]);
-      setLoading(false);
-    }, 1000);
-  }, []);
-
-  useEffect(() => {
-    getBoardList(sort);
-  }, [sort, getBoardList]);
-
-  const handleChangeSort = (type: SortType) => {
-    if (loading) return;
-    setSort(type);
-    getBoardList(type);
-  };
-  return (
-    <div>
-      <div>
-        <Badge
-          bg={sort === "recent" ? "primary.100" : "gray.100"}
-          color={sort === "recent" ? "primary.500" : "gray.400"}
-          borderColor={sort === "recent" ? "primary.500" : "gray.400"}
-          border={sort === "recent" ? "1px solid" : "none"}
-          borderRadius={"md"}
-          paddingY={"1px"}
-          mr={"8px"}
-          onClick={() => handleChangeSort("recent")}
-        >
-          최신순
-        </Badge>
-        <Badge
-          bg={sort === "popular" ? "primary.100" : "gray.100"}
-          color={sort === "popular" ? "primary.500" : "gray.400"}
-          borderColor={sort === "popular" ? "primary.500" : "gray.400"}
-          border={sort === "popular" ? "1px solid" : "none"}
-          borderRadius={"md"}
-          paddingY={"1px"}
-          onClick={() => handleChangeSort("popular")}
-        >
-          인기순
-        </Badge>
-      </div>
-      {loading ? (
-        "loading..."
-      ) : (
-        <BoardStack>
-          {clubBoardList.map(el => (
-            <FreeBoardItem
-              key={el.id}
-              comments={el.comments}
-              likes={el.likes}
-              date="2022-12-12"
-              views={el.views}
-              title={el.title}
-              onClick={() => router.push(`/board/${el.id}`)}
-            />
-          ))}
-        </BoardStack>
-      )}
-    </div>
-  );
-}
--- pages/board/club/index.tsx
+++ pages/board/club/index.tsx
@@ -1,71 +1,14 @@
-import React, { useEffect, useState } from "react";
-
-import { useRouter } from "next/router";
-
 import BoardFAB from "@/components/board/BoardFAB";
-import BoardStack from "@/components/board/BoardStack";
-import FreeBoardItem from "@/components/board/FreeBoardItem";
 import { AppContainer, Header } from "@/components/common";
 import IconSearch from "@/components/icons/IconSearch";
 
+import { AllPosts } from "../free";
+
 function ClubMain() {
-  const [freeBoardList, setFreeBoardList] = useState<any[]>([]);
-  useEffect(() => {
-    (async function () {
-      await setTimeout(() => {
-        setFreeBoardList([
-          {
-            id: 1,
-            title: "제목",
-            description: "본문",
-            comments: 24,
-            likes: 3,
-            date: "2022-12-12",
-          },
-          {
-            id: 2,
-            title: "제목",
-            description: "본문",
-            comments: 24,
-            likes: 3,
-            date: "2022-12-12",
-          },
-          {
-            id: 3,
-            title: "제목",
-            description: "본문",
-            comments: 24,
-            likes: 3,
-            date: "2022-12-12",
-          },
-        ]);
-      }, 1000);
-    })();
-  }, []);
-
-  const router = useRouter();
-
   return (
     <AppContainer>
       <Header back title="동아리" rightNode={<IconSearch />} />
-      <div>
-        <button>최신순</button>
-        <button>인기순</button>
-      </div>
-      <BoardStack>
-        {freeBoardList.map(boardItem => (
-          <FreeBoardItem
-            key={boardItem?.id}
-            views={23}
-            title={boardItem?.title}
-            description={boardItem?.description}
-            comments={boardItem?.comments}
-            likes={boardItem?.likes}
-            date="2022-12-12"
-            onClick={() => router.push(`/board/${boardItem?.id}`)}
-          />
-        ))}
-      </BoardStack>
+      <AllPosts />
       <BoardFAB />
     </AppContainer>
   );
