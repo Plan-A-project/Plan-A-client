2552607dea901a509b7586e797b8310ba3ec4fac
jimmy neutron <95525638+dlwnstjrzz@users.noreply.github.com>
2023-07-30 12:50:59 +0900

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,7 @@ export default function Document() {
           type="text/css"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css"
         />
-       <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+       <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
 
       </Head>
       <body>
