0744631a397ff83dd10269745ff074be0bb8ad12
noah <qny123@naver.com>
2023-09-13 22:53:54 +0800

    chore: 페이지 설정 오류 수정

--- components/board/PostsList.tsx
+++ components/board/PostsList.tsx
@@ -17,6 +17,7 @@ import { useRecoilState } from "recoil";
 import { boardListState } from "@/state/atoms/board/boardState";
 import { scrollPositionState } from "@/state/atoms/board/boardState";
 import { RepeatIcon } from "@chakra-ui/icons";
+import axios from "axios";
 
 type OrderType = "recent" | "popular";
 
@@ -36,6 +37,8 @@ const PostsList = ({
   const [scrollPosition, setScrollPosition] =
     useRecoilState(scrollPositionState);
   const boardListResponse = useBoardList({ boardId, order, page, type });
+  const firstPageList: any = useBoardList({ boardId, order, page: 1, type });
+  const [rotate, setRotate] = useState(false);
 
   // const [loading, setLoading] = useState<boolean>(false);
   // useEffect(() => {
@@ -54,6 +57,31 @@ const PostsList = ({
   //     window.removeEventListener("scroll", handleScroll);
   //   };
   // }, [loading]);
+  const handleReload = async () => {
+    setRotate(true); // 애니메이션 시작
+
+    const { data } = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}boards/${boardId}/posts`,
+      {
+        params: {
+          type,
+          page: 1,
+          order,
+          size: 20,
+        },
+      },
+    );
+    // 리로드 시 첫페이지 불러와서 리턴
+    setBoardInfo((p: any) => {
+      return {
+        ...p,
+        [boardId]: {
+          ...p[boardId],
+          [type]: data.posts,
+        },
+      };
+    });
+  };
   const handleChangeOrder = (orderType: OrderType) => {
     if (boardInfo[boardId] === null) return;
     setPage(1);
@@ -63,19 +91,44 @@ const PostsList = ({
       setBoardInfo((p: any) => {
         return {
           ...p,
-          [boardId]: { ...p[boardId], [type]: [] },
+          [boardId]: { ...p[boardId], [type]: [], type: orderType },
         };
       });
     }
   };
+  useEffect(() => {
+    if (rotate) {
+      const timer = setTimeout(() => setRotate(false), 2000); // 애니메이션이 끝난 후에 상태를 초기화합니다.
+      return () => clearTimeout(timer);
+    }
+  }, [rotate]);
 
+  useEffect(() => {
+    if (boardListResponse && boardInfo[boardId][type] && firstPageList) {
+      // 만약 새로운 글이 올라오면 전역상태 첫글과 서버에서 받아온 첫글을 비교하여 다시 렌더해줌
+      if (boardInfo[boardId][type][0]?.postId !== firstPageList[0]?.postId) {
+        // location.reload();
+
+        setBoardInfo((p: any) => {
+          return {
+            ...p,
+            [boardId]: {
+              ...p[boardId],
+              [type]: firstPageList,
+            },
+          };
+        });
+      }
+    }
+  }, [firstPageList]);
   useEffect(() => {
     // 게시글 리스트가 Recoil 상태에 없을 경우에만 API 호출로 데이터를 가져옵니다.
     if (boardListResponse === null) return;
     if (boardListResponse?.length < 20) {
       setIsFinish(true);
     }
-    if (boardListResponse && boardInfo[boardId][type]) {
+    if (boardListResponse && boardInfo[boardId][type] && firstPageList) {
+      // 만약 새로운 글이 올라오면 전역상태 첫글과 서버에서 받아온 첫글을 비교하여 다시 렌더해줌
       // 전역상태와 방금 받아온 글 리스트가 다를 때(즉 새로운 페이지를 불러왔을 때) 및 마지막 페이지가 아닐때(20으로 나눠질 때)
       if (
         boardInfo[boardId][type][0]?.postId !== boardListResponse[0]?.postId &&
@@ -96,12 +149,12 @@ const PostsList = ({
 
   // 게시글 진입 후 다시 리스트로 돌아올 시 페이지를 설정해줘야함(매번 1이면 안됨)
   useEffect(() => {
-    setBoardInfo((p: any) => {
-      return {
-        ...p,
-        [boardId]: { ...p[boardId], type: "recent" },
-      };
-    });
+    // setBoardInfo((p: any) => {
+    //   return {
+    //     ...p,
+    //     [boardId]: { ...p[boardId], type: "recent" },
+    //   };
+    // });
     setPage(Math.floor(boardInfo[boardId][type].length / 20) + 1);
   }, []);
 
@@ -179,8 +232,9 @@ const PostsList = ({
         </Box>
         <RepeatIcon
           boxSize={"22px"}
-          onClick={() => location.reload()}
+          onClick={handleReload}
           color={"primary.normal"}
+          style={rotate ? { animation: "rotateIcon 2s linear" } : {}}
         />
       </Flex>
       {!boardInfo[boardId][type]?.length &&
