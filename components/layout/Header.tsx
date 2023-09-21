78d23a2e58735175df2dd56a18ee0ce469769166
noah <qny123@naver.com>
2023-09-21 11:29:29 +0800

    chore: 헤더 색상 변경

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -7,7 +7,7 @@ import { searchFunctionFactory, testAutocompleteFunction } from "@/utils/utils";
 export default function Header() {
   const testSearchFunction = searchFunctionFactory("익명게시판");
   return (
-    <Flex bg={"#FFFFFF"} justify="space-between" p={"0.6rem 1rem"}>
+    <Flex bg={"#F7F8FA"} justify="space-between" p={"0.6rem 1rem"}>
       <Logo />
       <Flex justify="space-between">
         <SearchModal
