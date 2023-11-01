30c338fc9c6e242f3de6bb80f8a8459b163859c0
noah <qny123@naver.com>
2023-11-02 04:23:06 +0800

    chore: 일일 팁 멘트 추가

--- components/main/TextBanner.tsx
+++ components/main/TextBanner.tsx
@@ -6,12 +6,16 @@ export default function TextBanner() {
   return (
     <Flex
       color={"background.600"}
-      fontSize={"20px"}
+      fontSize={"18px"}
       fontWeight={"600"}
       lineHeight={"24px"}
+      textStyle={"subtitle1"}
     >
       <FireIcon />
-      <Text ml={"12px"}>지금 가장 인기있는 게시물!</Text>
+      <Text ml={"12px"}>
+        알고 계셨나요?
+        <br /> 공안이 학교에 들어올 수도 있답니다!
+      </Text>
     </Flex>
   );
 }
