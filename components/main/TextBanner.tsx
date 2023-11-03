3a51c327a7e72bf3c86da9355be9590855423e65
noah <qny123@naver.com>
2023-11-04 02:53:32 +0800

    chore: 틈새 정보 구현

--- components/main/TextBanner.tsx
+++ components/main/TextBanner.tsx
@@ -1,21 +1,19 @@
 import { Flex, Text } from "@chakra-ui/layout";
 
-import { FireIcon } from "../icons";
+import HoneyIcon from "../icons/HoneyIcon";
 
 export default function TextBanner() {
   return (
-    <Flex
-      color={"background.600"}
-      fontSize={"18px"}
-      fontWeight={"600"}
-      lineHeight={"24px"}
-      textStyle={"subtitle1"}
-    >
-      <FireIcon />
-      <Text ml={"12px"}>
-        알고 계셨나요?
-        <br /> 공안이 학교에 들어올 수도 있답니다!
+    <>
+      <Flex color={"background.600"} align={"center"} mb={"2px"}>
+        <HoneyIcon />
+        <Text ml={1} textStyle={"subtitle2"}>
+          틈새 꿀정보
+        </Text>
+      </Flex>
+      <Text textStyle={"body1"}>
+        알고 계셨나요? 위챗페이 연간 한도는 10만 위안이랍니다!
       </Text>
-    </Flex>
+    </>
   );
 }
