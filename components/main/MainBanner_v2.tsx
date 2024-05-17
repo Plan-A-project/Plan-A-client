f3277034adcede29d204197952da935f1b20aaae
noah <qny123@naver.com>
2024-05-17 15:07:44 +0800

    chore: 아이디어 해커톤 배너

--- components/main/MainBanner_v2.tsx
+++ components/main/MainBanner_v2.tsx
@@ -21,6 +21,7 @@ const MainBanner_v2 = () => {
     setCarouselItem([
       // { title: "클라이밍 이벤트 배너", src: "/assets/event_climbing.jpg" },
       // { title: "기본 배너", src: "/assets/event_banner_final.jpg" },
+      { title: "해커톤 배너", src: "/assets/event_banner_hackathon.jpg" },
       { title: "지식 인플리 배너", src: "/assets/event_knowledge3.gif" },
       { title: "기본 배너", src: "/assets/event_banner_market3.jpg" },
     ]);
@@ -74,9 +75,12 @@ const MainBanner_v2 = () => {
         renderIndicator={CustomIndicator}
         onClickItem={(index, item) => {
           if (index === 0) {
-            router.push("/knowledge");
+            router.push("/posting/2/2186");
           }
           if (index === 1) {
+            router.push("/knowledge");
+          }
+          if (index === 2) {
             router.push("/board/free");
           }
         }}
